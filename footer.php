e1d8d56448a1d8b4db6432a4fa6b70c9e2b548a5
Anurak Sumranphan <a.sumranphan@gmail.com>
2019-09-13 16:41:34 +0700

    feat: add a start theme footer file

---
+++ footer.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package WordPress_Theme_Starter_Kit
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+wp_footer();
+?>
+    </body>
+</html>
