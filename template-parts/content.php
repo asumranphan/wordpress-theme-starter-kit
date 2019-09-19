75688b6dd9b273da140bd7a6d68bcc5d982992f9
Anurak Sumranphan <a.sumranphan@gmail.com>
2019-09-19 11:51:26 +0700

    feat: add a default display post meta

--- template-parts/content.php
+++ template-parts/content.php
@@ -28,6 +28,29 @@ if ( ! defined( 'ABSPATH' ) ) {
             ?>
         </header>
 
+        <div class="entry-meta mb-2">
+            <span class="byline">
+                <i class="fas fa-user-circle"></i>
+                <span class="screen-reader-text">
+                    <?php esc_html_e( 'Posted by', 'wordpress-theme-starter-kit' ); ?>
+                </span>
+                <span class="author vcard">
+                    <a class="url fn n" href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>">
+                        <?php echo esc_html( get_the_author() ); ?>
+                    </a>
+                </span>
+            </span>
+
+            <span class="posted-on">
+                <i class="far fa-clock"></i>
+                <a class="posted-on__link" href="<?php echo esc_url( get_permalink() ); ?>" rel="bookmark">
+                    <time class="entry-date published updated" datetime="<?php echo esc_attr( get_the_date( DATE_W3C ) ); ?>">
+                        <?php echo esc_html( get_the_date() ); ?>
+                    </time>
+                </a>
+            </span>
+        </div>
+
         <div class="entry-content">
             <?php the_excerpt(); ?>
         </div>
