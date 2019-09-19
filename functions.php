f88a41438b4db107eb0bae6731c9c751c35d33fa
Anurak Sumranphan <a.sumranphan@gmail.com>
2019-09-19 11:23:09 +0700

    feat: change heading tags to "h3"

--- functions.php
+++ functions.php
@@ -95,8 +95,8 @@ function wordpress_theme_starter_kit_sidebar() {
 			'description'   => __( 'Add widgets here to appear in your sidebar.', 'wordpress-theme-starter-kit' ),
 			'before_widget' => '<section id="%1$s" class="widget %2$s">',
 			'after_widget'  => '</section>',
-			'before_title'  => '<h2 class="widget-title">',
-			'after_title'   => '</h2>',
+			'before_title'  => '<h3 class="widget-title">',
+			'after_title'   => '</h3>',
 		)
     );
 }
