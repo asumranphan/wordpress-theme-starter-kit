69d81f2ce67cfc993f9553c7d8349a4ee20c2a39
Anurak Sumranphan <a.sumranphan@gmail.com>
2019-10-02 11:04:16 +0700

    feat: add showing template when no content

--- index.php
+++ index.php
@@ -23,31 +23,38 @@ get_header();
         <main id="main" class="<?php echo esc_attr( is_singular() ? 'site-main' : 'site-main posts-list' ); ?>" role="main">
 
             <?php
-            while ( have_posts() ) {
-                the_post();
+            if ( have_posts() ) {
 
-                if ( is_singular() ) {
-                    get_template_part( 'template-parts/content', 'single' );
-                } else {
-                    get_template_part( 'template-parts/content' );
+                while ( have_posts() ) {
+                    the_post();
+
+                    if ( is_singular() ) {
+                        get_template_part( 'template-parts/content', 'single' );
+                    } else {
+                        get_template_part( 'template-parts/content' );
+                    }
                 }
-            }
 
-            the_posts_pagination(
-                array(
-                    'mid_size'  => 2,
-                    'prev_text' => sprintf(
-                        '%s <span class="nav-prev-text">%s</span>',
-                        '<i class="fas fa-chevron-left mr-1"></i>',
-                        __( 'Newer posts', 'wordpress-theme-starter-kit' )
-                    ),
-                    'next_text' => sprintf(
-                        '<span class="nav-next-text">%s</span> %s',
-                        __( 'Older posts', 'wordpress-theme-starter-kit' ),
-                        '<i class="fas fa-chevron-right ml-1"></i>'
-                    ),
-                )
-            );
+                the_posts_pagination(
+                    array(
+                        'mid_size'  => 2,
+                        'prev_text' => sprintf(
+                            '%s <span class="nav-prev-text">%s</span>',
+                            '<i class="fas fa-chevron-left mr-1"></i>',
+                            __( 'Newer posts', 'wordpress-theme-starter-kit' )
+                        ),
+                        'next_text' => sprintf(
+                            '<span class="nav-next-text">%s</span> %s',
+                            __( 'Older posts', 'wordpress-theme-starter-kit' ),
+                            '<i class="fas fa-chevron-right ml-1"></i>'
+                        ),
+                    )
+                );
+
+            } else {
+
+                get_template_part( 'template-parts/content', 'none' );
+            }
             ?>
 
         </main>
